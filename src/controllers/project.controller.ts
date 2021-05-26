774b7e4234ced4362aa527cd93982bbdb0a5266d
Materuilist <50238654+Materuilist@users.noreply.github.com>
2021-05-26 11:27:26 +0500

    Запрос проектов юзера возвращает еще аггрегированную инфу о датах, роль юзера в проекте

--- src/controllers/project.controller.ts
+++ src/controllers/project.controller.ts
@@ -5,19 +5,57 @@ import Project from "../model/project.model";
 import UserProjectRole from "../model/user-project-role.model";
 import Error from "../entities/shared/error";
 import { UserRoleEnum } from "../constants/user.role.eumn";
+import UserRole from "../model/user-role.model";
+import sequalize from "utils/database";
+import { col, fn, literal } from "sequelize";
 
 export const getProjects: RequestHandler = async (req, res, next) => {
     const { user } = req.body;
 
     const projects = await Project.findAll({
+        attributes: {
+            include: [
+                [
+                    literal(
+                        "(SELECT min(tasks.maxStart) AS maxStart FROM projects JOIN tasks)"
+                    ),
+                    "maxStart",
+                ],
+                [
+                    literal(
+                        "(SELECT max(tasks.maxEnd) AS maxEnd FROM projects JOIN tasks)"
+                    ),
+                    "maxEnd",
+                ],
+                [
+                    literal(
+                        "(SELECT min(tasks.minStart) AS minStart FROM projects JOIN tasks)"
+                    ),
+                    "minStart",
+                ],
+                [
+                    literal(
+                        "(SELECT max(tasks.minEnd) AS minEnd FROM projects JOIN tasks)"
+                    ),
+                    "minEnd",
+                ],
+            ],
+        },
         include: [
             {
                 model: UserProjectRole,
                 as: "usersRoles",
-                attributes: ["userId"],
+                attributes: ["id"],
                 where: {
                     userId: user.id,
                 },
+                include: [
+                    {
+                        model: UserRole,
+                        as: "role",
+                        attributes: ["name"],
+                    },
+                ],
             },
         ],
     });
